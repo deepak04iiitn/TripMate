ae49ffe969dac9c76a9ef42ddbafba86e21f5dba
deepak04iiitn <dky422003@gmail.com>
2024-08-18 18:45:09 +0530

    added the global font family

---
+++ Travel-Planner/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text style={{
+          fontSize : 30,
+          fontFamily : 'outfit-bold'
+      }}>Edit app/index.tsx to edit this screen.</Text>
+
+    </View>
+  );
+}
